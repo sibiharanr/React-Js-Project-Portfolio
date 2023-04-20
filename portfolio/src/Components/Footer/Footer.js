c393ab71f8382c39cdd380bc9c3765ca8525d31a
Sibiharan <124172962+sibiharanr@users.noreply.github.com>
2023-04-20 19:24:34 +0530

    Create Footer.js

---
+++ portfolio/src/Components/Footer/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div>
+        <section>
+        <div className="bird-container one">
+            <div className="bird one1"></div>
+        </div>
+        <div className="bird-container two">
+            <div className="bird two1"></div>
+        </div>
+
+        <div className="air air1"></div>
+        <div className="air air2"></div>
+        <div className="air air3"></div>
+        <div className="air air4"></div>
+        </section>
+        <center className="app" style={{display:"flex"}}>
+           <p>. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+           . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+            2023 @ Created with ❣️ by Sibi
+           . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+           . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+           </p>
+        </center>
+    </div>
+  )
+}
+
+export default Footer
